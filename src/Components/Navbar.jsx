66e4cc37c8d06cce27486bce2d337204aa315c75
coderbekhub <coderbek@mail.ru>
2024-04-16 12:57:06 +0500

    ...

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,24 +3,38 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-      <section className='navBar'>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <section className='navSection'>
+        <nav class="navbar navbar-expand-lg bg-light">
           <div className="container">
-            <div>
-              <a class="navbar-brand" href="#">Navbar</a>
-            </div>
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+            <div className='navContent'>
+              <div className='navBrand'>
+                <a class="navbar-brand" href="/">Masteruz</a>
+              </div>
+              
+              <div className='navBurger'>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </div>
+
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  <a class="nav-link" href="#">Features</a>
-                  <a class="nav-link" href="#">Pricing</a>
-                </div>
+                <ul class="navbar-nav">
+                  <li>
+                    <a class="nav-link active" aria-current="page" href="#">Стиральные машины</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="#">Кондиционеры</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="#">Холодильники</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="tel:+998935992504">Позвонить мастеру</a>
+                  </li>
+                </ul>
               </div>
             </div>
+            
           </div>
         </nav>      
       </section>
