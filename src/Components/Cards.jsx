9502a3d0a4303933d20db4fcab6b029a81fa7eba
coderbekhub <coderbek@mail.ru>
2024-05-07 17:47:52 +0500

    ...

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import GarantIcon from '../img/garant.svg'
+import Diagnostics from '../img/diagnostika.png'
+import SettingsIcon from '../img/settings.svg'
+import OpitIcon from '../img/opit.svg'
 
 export default function Cards() {
   return (
@@ -8,34 +12,34 @@ export default function Cards() {
           <div className='row cardsContent'>
             <div className="cards col-md-3 col-12">
               <div>
-                <img src='' alt="location icon" />
+                <img id='garantIcon' src={GarantIcon} alt="Garant icon" />
               </div>
-              <h2>Выезд на дом</h2>
-              <p>Позвоните нам и назначьте вашу локацию и наш мастер выедет к Вам в назначенное время</p> 
+              <h2>Гарантия до 3-х лет</h2>
+              <p>Работаем по договору и выдаём гарантийный талон сроком до 3-х лет</p> 
             </div>
 
             <div className="cards col-md-3 col-12">
               <div>
-                <img src='' alt="location icon" />
+                <img id='diagnosticsIcon' src={Diagnostics} alt="Diagnostics icon" />
               </div>
-              <h2>Выезд на дом</h2>
-              <p>Позвоните нам и назначьте вашу локацию и наш мастер выедет к Вам в назначенное время</p> 
+              <h2>Бесплатная диагностика</h2>
+              <p>0 сум за диагностику в случае выполненного ремонта</p> 
             </div>
 
             <div className="cards col-md-3 col-12">
               <div>
-                <img src='' alt="location icon" />
+                <img id='settingsIcon' src={SettingsIcon} alt="Settings icon" />
               </div>
-              <h2>Выезд на дом</h2>
-              <p>Позвоните нам и назначьте вашу локацию и наш мастер выедет к Вам в назначенное время</p> 
+              <h2>Оригинальные запчасти</h2>
+              <p>В наличие 5000+ качественных запчастей, и устанавливаем оригинальные комплектующие</p> 
             </div>
 
             <div className="cards col-md-3 col-12">
               <div>
-                <img src='' alt="location icon" />
+                <img id='opitIcon' src={OpitIcon} alt="Opit icon" />
               </div>
-              <h2>Выезд на дом</h2>
-              <p>Позвоните нам и назначьте вашу локацию и наш мастер выедет к Вам в назначенное время</p> 
+              <h2>15 летний опыт</h2>
+              <p>Ремонтируем технику на протяжение 15 лет, а это более 10,000 довольных клиентов</p> 
             </div>
           </div>
         </div>
